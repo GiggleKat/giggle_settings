8422b51f8299922f40b04c63e318ae3300fb0ffe
Patrick <gigglekatrom@gmail.com>
2014-11-04 18:42:53 +0100

    Made Giggly

--- src/com/android/settings/beanstalk/About.java
+++ src/com/android/settings/beanstalk/About.java
@@ -62,7 +62,7 @@ public class About extends SettingsPreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSiteUrl) {
-            launchUrl("http://www.teambliss.x10host.com");
+            launchUrl("http://gigglekat.com");
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
